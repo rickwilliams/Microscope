615f61a94a6c997acc8347c50517936e334fdc6b
rickwilliams <rick@pixelpod.co.uk>
2014-06-15 15:45:32 +0100

    voting functionality

--- client/views/posts/post_edit.js
+++ client/views/posts/post_edit.js
@@ -4,7 +4,7 @@ var currentPostId = this._id;
 var postProperties = {
 url: $(e.target).find('[name=url]').val(), title: $(e.target).find('[name=title]').val()
 }
-Posts.update(currentPostId, {$set: postProperties}, function(error) { 
+Posts.update(currentPostId, {$set: postProperties}, function(error) {
 	if (error) {
     // display the error to the user
 Errors.throw(error.reason); } else {
@@ -12,6 +12,9 @@ Errors.throw(error.reason); } else {
       }
 }); },
 'click .delete': function(e) { e.preventDefault();
-if (confirm("Delete this post?")) { var currentPostId = this._id; Posts.remove(currentPostId); Router.go('postsList');
+if (confirm("Delete this post?")) {
+  var currentPostId = this._id;
+  Posts.remove(currentPostId);
+  Router.go('home');
 } }
 });
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,3 +1,5 @@
-Template.postsList.helpers({ posts: function() {
-return Posts.find({}, {sort: {submitted: -1}}); }
+Template.postsList.helpers({
+  posts: function() {
+return Posts.find({}, {sort: {submitted: -1}});
+}
 });
